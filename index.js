7414ab070ab527ab1fad12baf529389b9eeea874
Mikołaj Walanus <mikolaj@walanus.com>
2020-07-19 14:45:19 +0000

    ES6 vesrion with CLASS

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function Person(name) {
-  this.name = name;
-  this.greet = function (herName) {
+// function Person(name) {
+//   this.name = name;
+//   this.greet = function (herName) {
+//     return `Hello ${herName}, my name is ${this.name}`
+//   }
+// }
+
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  greet(herName) {
     return `Hello ${herName}, my name is ${this.name}`
   }
 }
