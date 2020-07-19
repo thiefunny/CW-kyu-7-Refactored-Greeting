29963e055844222dc454a3b325cfa157fd90adc4
Mikołaj Walanus <mikolaj@walanus.com>
2020-07-19 14:39:29 +0000

    best version

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function Person(name) {
-  this.name = name
-}
-
-Person.prototype.greet = function (herName) {
+  this.name = name;
+  this.greet = function (herName) {
     return `Hello ${herName}, my name is ${this.name}`
   }
+}
 
 let joe = new Person("Joe");
 
